b14b140ce0752a445f43789a410a227ab20e566e
zhanjun <zhanjun@gmail.com>
2024-04-03 11:31:25 +0800

    add pycococheck-Automine.ipynb

---
+++ trainall.sh
@@ -0,0 +1,29 @@
+
+sleep 5
+yolo train data=automine-2d.yaml model=rtdetr-l.pt imgsz=1024 batch=2 epochs=30
+
+
+sleep 5
+
+yolo train data=automine-2d.yaml model=yolov8s.pt imgsz=1024 batch=4 epochs=30
+
+
+sleep 5
+
+yolo train data=automine-2d.yaml model=yolov8l.pt imgsz=1024 batch=4 epochs=30
+
+
+sleep 5
+
+yolo train data=automine-2d.yaml model=yolov9e.pt imgsz=1024 batch=2 epochs=30
+
+
+sleep 5
+
+yolo train data=automine-2d.yaml model=yolov9c.pt  batch=8 epochs=30
+
+
+sleep 5
+
+#yolo train data=automine-2d.yaml model=yolov8s-rtdetr.pt  imgsz=1024 batch=4
+
